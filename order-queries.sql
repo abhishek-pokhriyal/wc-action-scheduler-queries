6939f8b978f67d43875a632b40add38f359b9c3c
Abhishek Pokhriyal <pokhi@coloredcow.com>
2020-05-27 12:47:41 +0530

    Create order-queries.sql

---
+++ order-queries.sql
@@ -0,0 +1,17 @@
+# Count the number of line items for some orders.
+# In the below example, I've used a sub-query to find out
+# all the subscriptions that were created after a particular time.
+# That sub-query can be modified, however you want, keeping in mind that
+# it's output should be a single column with multiple rows containing order IDs.
+
+SELECT order_id,
+       count(*)
+FROM wp_woocommerce_order_items
+WHERE order_id IN
+    (SELECT ID
+     FROM wp_posts
+     WHERE post_type = 'shop_subscription'
+       AND post_date_gmt > '2020-05-21 00:00:00')
+  AND order_item_type = 'line_item'
+GROUP BY order_id
+ORDER BY order_id;
