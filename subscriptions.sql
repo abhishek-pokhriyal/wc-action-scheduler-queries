95327a4a66c75010a5ae5a870f3340dfa2c82abf
Abhishek Pokhriyal <pokhi@coloredcow.com>
2020-06-17 19:33:40 +0530

    Update subscriptions.sql

--- subscriptions.sql
+++ subscriptions.sql
@@ -15,3 +15,20 @@ FROM
 LEFT JOIN wp_woocommerce_order_itemmeta itemmeta ON subscription_items.order_item_id = itemmeta.order_item_id
 WHERE meta_key = '_wcsatt_scheme'
 ORDER BY subscription_items.post_date ASC;
+
+
+
+# Customer and the number of active subscriptions they have
+SELECT subscription_meta.meta_value AS 'Customer ID',
+       customer.user_email AS 'User email',
+       count(*) AS 'Active subscriptions'
+FROM
+  (SELECT *
+   FROM wp_posts
+   WHERE post_type = 'shop_subscription'
+     AND post_status = 'wc-active') active_subscriptions
+LEFT JOIN wp_postmeta subscription_meta ON active_subscriptions.ID = subscription_meta.post_id
+LEFT JOIN wp_users customer ON subscription_meta.meta_value = customer.ID
+WHERE meta_key = '_customer_user'
+GROUP BY subscription_meta.meta_value
+ORDER BY count(*) DESC;
